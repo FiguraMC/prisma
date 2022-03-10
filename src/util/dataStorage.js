a1bb0e2534d1c6f0c551247acbafac10765eb33f
Manuel-3 <58179488+Manuel-3@users.noreply.github.com>
2022-03-10 18:04:01 +0100

    DataStorage safety update

--- src/util/dataStorage.js
+++ src/util/dataStorage.js
@@ -2,26 +2,72 @@ const fs = require('fs');
 const path = require('path');
 
 const storageDirectoryPath = path.join(__dirname, '../..', 'storage');
-const storageFilePath = path.join(storageDirectoryPath, 'storage.json');
+const file_old = path.join(storageDirectoryPath, 'storage.json');
+const file = [path.join(storageDirectoryPath, 'storage0.json'), path.join(storageDirectoryPath, 'storage1.json')];
+
+let flip = 0;
 
 // Used for permanent storage
 exports.storage = {};
 
 /**
- * Load data storage from file
+ * Load data storage from file.
+ * Uses the newer one of the two files or,
+ * if one is corrupted, the working one.
  */
 function load() {
-    if (!fs.existsSync(storageFilePath)) return save();
-    exports.storage = JSON.parse(fs.readFileSync(storageFilePath).toString(), reviver);
-    save();
+    fs.mkdirSync(storageDirectoryPath, { recursive: true });
+    if (!fs.existsSync(file[0])) fs.writeFileSync(file[0], '{}');
+    if (!fs.existsSync(file[1])) fs.writeFileSync(file[1], '{}');
+    if (fs.existsSync(file_old)) {
+        // Move old storage.json file over to the new storage0 and storage1 alternating system
+        exports.storage = JSON.parse(fs.readFileSync(file_old).toString(), reviver);
+        save();
+        fs.unlinkSync(file_old);
+        return;
+    }
+    // Attempt reading both files, treat as empty with timestamp 0 on error
+    let st0;
+    let st1;
+    try {
+        st0 = JSON.parse(fs.readFileSync(file[0]).toString(), reviver);
+    }
+    catch (error) {
+        st0 = { timestamp: 0 };
+    }
+    try {
+        st1 = JSON.parse(fs.readFileSync(file[1]).toString(), reviver);
+    }
+    catch (error) {
+        st1 = { timestamp: 0 };
+    }
+    // Use the newer one of the two files
+    if (st0.timestamp > st1.timestamp) {
+        exports.storage = st0;
+        flip = 1;
+    }
+    else {
+        exports.storage = st1;
+        flip = 0;
+    }
 }
 
 /**
- * Save data storage to file
+ * Save data storage to file.
+ * Uses two files alternating between them in
+ * case the process gets killed or the bot crashes
+ * during the writing process.
  */
 function save() {
     fs.mkdirSync(storageDirectoryPath, { recursive: true });
-    fs.writeFileSync(storageFilePath, JSON.stringify(exports.storage, replacer));
+    exports.storage.timestamp = Date.now();
+    fs.writeFileSync(file[flip], JSON.stringify(exports.storage, replacer));
+    if (flip == 0) {
+        flip = 1;
+    }
+    else {
+        flip = 0;
+    }
 }
 
 /**
