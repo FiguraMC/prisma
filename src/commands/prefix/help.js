8d2d7d214419d631a5286ec115b350bd27377e6f
Manuel-3 <58179488+Manuel-3@users.noreply.github.com>
2022-07-21 20:05:45 +0200

    Port help command

--- src/commands/prefix/help.js
+++ src/commands/prefix/help.js
@@ -1,43 +1,48 @@
-const Discord = require('discord.js'); // eslint-disable-line no-unused-vars
+const Argument = require('../parser/argument');
 const utility = require('../../util/utility');
 
 // Help command
-// Automatically gets information about other commands to build a reply
+// Gets information about other commands to build a reply
 module.exports = {
     name: 'help',
-    usage: '`?help` - Shows this message.',
+    description: 'Shows this message.',
     allowInOtherGuilds: true,
-    /**
-     * 
-     * @param {Discord.Message} message 
-     */
-    async execute(message) {
+    overloads: [
+        {
+            arguments: [],
+            /**
+            * @param {import('discord.js').Message} message 
+            */
+            execute: async (message) => {
+                const isModerator = utility.isModerator(message.member);
 
-        const isModerator = utility.isModerator(message.member);
+                const commands = { text:'**Available commands**\n' };
+                const modCommands = { text:'**Moderator commands**\n' };
 
-        let commands = '**Available commands**\n';
-        let modCommands = '**Moderator commands**\n';
+                message.client.slashCommands.forEach(command => {
+                    if (message.guild.id != process.env.MAIN_GUILD && !command.allowInOtherGuilds) return;
+                    const group = command.moderator ? modCommands : commands;
+                    group.text += `\`/${command.name}\` - ${command.description}\n`;
+                });
 
-        message.client.slashCommands.forEach(command => {
-            if (message.guild.id != process.env.MAIN_GUILD && !command.allowInOtherGuilds) return;
-            if (command.moderator) {
-                modCommands += command.usage + '\n';
-            }
-            else {
-                commands += command.usage + '\n';
-            }
-        });
+                message.client.prefixCommands.forEach(command => {
+                    if (message.guild.id != process.env.MAIN_GUILD && !command.allowInOtherGuilds) return;
+                    const group = command.moderator ? modCommands : commands;
+                    group.text += `\`${process.env.PREFIX}${command.name}\` - ${command.description}\n`;
+                });
 
-        message.client.prefixCommands.forEach(command => {
-            if (message.guild.id != process.env.MAIN_GUILD && !command.allowInOtherGuilds) return;
-            if (command.moderator) {
-                modCommands += command.usage + '\n';
-            }
-            else {
-                commands += command.usage + '\n';
-            }
-        });
-
-        message.reply({ embeds: [{ description: `${commands}\n${isModerator ? modCommands : ''}` }] });
-    },
+                message.reply({ embeds: [{ description: `${commands.text}\n${isModerator ? modCommands.text : ''}` }] });
+            },
+        },
+        {
+            arguments: [new Argument('command', 'command')],
+            /**
+            * @param {import('discord.js').Message} message 
+            * @param {import('../parser/argumentContainer')} args
+            */
+            execute: async (message, args) => {
+                message.reply(utility.buildEmbed(args.getValue('command').longDescription ?? args.getValue('command').description));
+            },
+        },
+    ],
 };
