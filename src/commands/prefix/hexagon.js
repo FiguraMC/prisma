3789775d02f6717f2147169f706cdf736fd2654b
Manuel-3 <58179488+Manuel-3@users.noreply.github.com>
2023-07-26 18:24:35 +0200

    Fix help command
    Some commands were not formatted correctly

--- src/commands/prefix/hexagon.js
+++ src/commands/prefix/hexagon.js
@@ -1,41 +1,47 @@
 const Discord = require('discord.js');
 const hexagonalImage = require('../../util/hexagonImage');
 const utility = require('../../util/utility');
+const Argument = require('../parser/argument');
 
 module.exports = {
     name: 'hexagon',
     description: 'Create a hexagonal profile picture.',
     allowInOtherGuilds: true,
-    /**
-     * 
-     * @param {import('discord.js').Message} message 
-     * @param {String[]} args 
-     */
-    async execute(message, args) { // eslint-disable-line no-unused-vars
+    overloads:[
+        {
+            arguments: [new Argument('image', 'image')],
+            /**
+             * 
+             * @param {import('discord.js').Message} message 
+             * @param {String[]} args 
+             */
+            async execute(message, args) { // eslint-disable-line no-unused-vars
 
-        let fetchedMember = undefined;
-        try {
-            if (args[0]) fetchedMember = await message.guild.members.fetch(args[0]);
-        }
-        // eslint-disable-next-line no-empty
-        catch { }
+                let fetchedMember = undefined;
+                try {
+                    if (args[0]) fetchedMember = await message.guild.members.fetch(args[0]);
+                }
+                // eslint-disable-next-line no-empty
+                catch { }
 
-        const member = fetchedMember || message.mentions.members.first();
+                const member = fetchedMember || message.mentions.members.first();
 
-        const url = member?.displayAvatarURL({ format: 'png' }) ?? utility.getURLs(args[0] ?? '')?.at(0) ?? message.attachments.first()?.url;
+                const url = member?.displayAvatarURL({ format: 'png' }) ?? utility.getURLs(args[0] ?? '')?.at(0) ?? message.attachments.first()?.url;
 
-        try {
-            const image = await hexagonalImage(url);
-            message.reply({
-                files: [new Discord.MessageAttachment(
-                    image,
-                    'avatar.png',
-                )],
-            });
-        }
-        catch (err) {
-            console.log(err);
-            message.reply('Couldn\'t find an image.');
-        }
-    },
+                try {
+                    const image = await hexagonalImage(url);
+                    message.reply({
+                        files: [new Discord.MessageAttachment(
+                            image,
+                            'avatar.png',
+                        )],
+                    });
+                }
+                catch (err) {
+                    console.log(err);
+                    message.reply('Couldn\'t find an image.');
+                }
+            },
+        },
+    ],
 };
--- src/commands/prefix/ping.js
+++ src/commands/prefix/ping.js
@@ -2,11 +2,16 @@ module.exports = {
     name: 'ping',
     description: 'Ping command for testing.',
     allowInOtherGuilds: true,
-    /**
-     * 
-     * @param {import('discord.js').Message} message 
-     */
-    async execute(message) {
-        await message.reply('Pong!');
-    },
+    overloads: [
+        {
+            arguments: [],
+            /**
+             * 
+             * @param {import('discord.js').Message} message 
+             */
+            async execute(message) {
+                await message.reply('Pong!');
+            },
+        },
+    ],
 };
