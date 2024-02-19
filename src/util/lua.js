8f097447fbddb07fe006786e107e97c7f36a2cf2
Manuel-3 <58179488+Manuel-3@users.noreply.github.com>
2024-02-19 18:19:46 +0100

    Remove lua heuristics
    (it didnt work and was just annoying)

--- src/util/lua.js
+++ src/util/lua.js
@@ -26,36 +26,6 @@ function preprocessLuaCode(luaCode) {
 }
 
 /**
- * Try to find lua code when no backticks formatting is used
- * Only detects starting from two lines of code, inline code is not detected
- * @param {string} text 
- * @returns string array of length 1 of potential lua code, if backticks are found return empty array
- */
-function detectPotentialLuaCode(text) {
-    if (text.includes('`')) return [];
-
-    const lines = text.split('\n');
-    let startIndex = null;
-    let endIndex = null;
-
-    for (let i = 0; i < lines.length; i++) {
-        // Simple heuristics to identify potential Lua code lines
-        if (lines[i].match(/(local|return|else|end)/) || lines[i].match(/function.*\(/) || lines[i].match(/if\b.+\bthen/) || lines[i].match(/(while|for)\b.+\bdo/) || lines[i].match(/[.:]\w+\(/)) {
-            if (startIndex === null) {
-                startIndex = i;
-            }
-            else {
-                endIndex = i;
-            }
-        }
-    }
-
-    if (startIndex !== null && endIndex !== null) return [lines.slice(startIndex, endIndex + 1).join('\n')];
-
-    return [];
-}
-
-/**
  * Find lua code in a string and check if it contains a syntax error
  * @param {string} text 
  * @returns string if error found, else null
@@ -63,7 +33,7 @@ function detectPotentialLuaCode(text) {
 function checkMessageForLuaError(text) {
 
     // Detect code blocks, or if not present try detecting potential lua code
-    const luaCodeBlocks = extractLuaCodeBlocks(text).concat(detectPotentialLuaCode(text));
+    const luaCodeBlocks = extractLuaCodeBlocks(text);
 
     // Check each individual code block for syntax errors
     let errors = '';
